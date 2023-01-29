67b981bcb4d45b472daf8e7d191de42e2b317ab1
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-01-30 03:19:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,85 @@
-import React, { useEffect } from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import HomePage from "./containers/HomePage";
-import ProductListPage from "./containers/ProductListPage";
-import { useDispatch, useSelector } from "react-redux";
-import { isUserLoggedIn, updateCart } from "./actions";
-import ProductDetailsPage from "./containers/ProductDetailsPage";
-import CartPage from "./containers/CartPage";
-import CheckoutPage from "./containers/CheckoutPage";
-import OrderPage from "./containers/OrderPage";
-import OrderDetailsPage from "./containers/OrderDetailsPage";
+import HomePage from './Pages/HomePage'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './Pages/Login'
+import Header from './components/Header/Header'
+import useToken from './hooks/useToken'
+// import Profile from './Pages/Profile'
+// import Cart from './Pages/Cart'
+import NotFound from './Pages/NotFound'
+import useRouter from './hooks/useRouter'
+import BecomeAseller from './Pages/Become-a-seller'
+import Category from './Pages/Category'
+import More from './Pages/More'
+const App = () => {
+  const {  setToken, isLogin, isINIT } = useToken()
 
-function App() {
-  const dispatch = useDispatch();
-  const auth = useSelector((state) => state.auth);
-
-  useEffect(() => {
-   if (!auth.authenticate) {
-      dispatch(isUserLoggedIn());
-    }
-  }, [auth.authenticate,dispatch]);
-
- useEffect(() => {
-   console.log("App.js - updateCart");
-   dispatch(updateCart());
- }, [auth.authenticate,dispatch]);
-
-
+  const { routes } = useRouter()
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/cart" component={CartPage} />
-          <Route path="/checkout" component={CheckoutPage} />
-          <Route path="/account/orders" component={OrderPage} />
-          <Route path="/order_details/:orderId" component={OrderDetailsPage} />
-          <Route
-            path="/:productSlug/:productId/p"
-            component={ProductDetailsPage}
-          />
-          <Route path="/:slug" component={ProductListPage} />
-        </Switch>
-      </Router>
-    </div>
-  );
+    <>
+      <Header isLogin={isLogin} />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/become-a-seller' element={<BecomeAseller />} />
+        <Route path='/more' element={<More />} />
+        <Route path='/category' element={<Category />} />
+        <Route path='/category/:pCat' element={<Category />} />
+        <Route path='/category/:pCat/:sCat' element={<Category />} />
+        <Route path='/category/:pCat/:sCat/:iCat' element={<Category />} />
+        {isINIT &&
+          (isLogin ? (
+            <>
+              {routes.map((route, index) => (
+                <Route
+                  key={index.toString()}
+                  path={route.Path}
+                  element={<route.Element {...route.props} />}
+                />
+              ))}
+              <Route path='/login' element={<Navigate to='/' replace />} />
+              <Route path='/not-found' element={<NotFound />} />
+              <Route path='*' element={<Navigate to='/not-found' replace />} />
+            </>
+          ) : (
+            <>
+              <Route path='/login' element={<Login setToken={setToken} />} />
+              {routes.map((route, index) => (
+                <Route
+                  key={index.toString()}
+                  path={route.Path}
+                  element={<Navigate to='/login' replace />}
+                />
+              ))}
+              <Route path='/not-found' element={<NotFound />} />
+              <Route path='*' element={<Navigate to='/not-found' replace />} />
+            </>
+          ))}
+        {/* {isINIT &&
+          (isLogin ? (
+            <>
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/login' element={<Navigate to='/' replace />} />
+              <Route path='/not-found' element={<NotFound />} />
+              <Route path='*' element={<Navigate to='/not-found' replace />} />
+            </>
+          ) : (
+            <>
+              <Route
+                path='/login'
+                element={<Login setToken={setToken}  />}
+              />
+              <Route
+                path='/profile'
+                element={<Navigate to='/login' replace />}
+              />
+              <Route path='/cart' element={<Navigate to='/login' replace />} />
+              <Route path='/not-found' element={<NotFound />} />
+              <Route path='*' element={<Navigate to='/not-found' replace />} />
+            </>
+          ))} */}
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
