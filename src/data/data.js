08bc39253871871c7ff4ad0c92945b4f31a6b81d
Sarika Karle <104686761+sarikapanmand@users.noreply.github.com>
2023-01-30 03:25:01 +0530

    Add files via upload

--- src/data/data.js
+++ src/data/data.js
@@ -1,1 +1,17 @@
-
+export const bannerdata = [
+  {
+    id: 1,
+    image:
+      'https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/5bca549eed255944.jpg?q=50',
+  },
+  {
+    id: 2,
+    image:
+      'https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/f840057752e26a18.jpg?q=50',
+  },
+  {
+    id: 3,
+    image:
+      'https://rukminim1.flixcart.com/fk-p-flap/3376/560/image/a08c670e4a17621a.jpeg?q=50',
+  },
+]
